b201c2f03f43d0c9308cb2dccb29d1b2e1a24300
Bilal Can Altındağ <bilalcanaltindag@hotmail.com>
2022-03-13 14:06:12 +0300

    MetotDemo

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi : " + musteri.MusteriName + musteri.MusteriSurname);
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi : " + musteri.MusteriName + musteri.MusteriSurname);
+        }
+
+        public void MusteriGuncelle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteriler Güncellendi : " + musteri.MusteriName + musteri.MusteriSurname); 
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MusteriSil = 1;
+            int MusteriEkle = 1;
+            int MusteriGuncelle = 1;
+
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriId = 1;
+            musteri1.MusteriName = "Bilal";
+            musteri1.MusteriSurname = "Altındağ";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriId = 2;
+            musteri2.MusteriName = "Efe";
+            musteri2.MusteriSurname = "Altındağ";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.MusteriId = 3;
+            musteri3.MusteriName = "Tamer";
+            musteri3.MusteriSurname = "Çetin";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri'nin Adı : " + musteri.MusteriName + " " + musteri.MusteriSurname);
+            }
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            if (MusteriSil == 1)
+            {
+                musteriManager.Delete(musteri1);
+            }
+
+            if (MusteriEkle == 1)
+            {
+                musteriManager.Add(musteri2);
+            }
+
+            if (MusteriGuncelle == 1)
+            
+                musteriManager.MusteriGuncelle(musteri3);
+            }
+
+
+
+
+        }
+    }
+
